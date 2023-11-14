12bc71c0092727bbcdd1091599768ca0ae0e99b0
Smoking_Elk <andre12go@mail.ru>
2023-11-14 17:47:42 +0300

    add initial files, add .gitignore

---
+++ model_1/main.py
@@ -0,0 +1,72 @@
+from plotter import make_plot
+from math import sqrt, atan2, cos, sin
+
+MOON_RADIUS = 1
+MOON_MASS = 0.5
+SHIP_MASS = 0.02
+G = 1
+
+COLOR = (53, 178, 241)
+DT = 0.01
+STEPS_COUNT = 8000
+
+RX0 = 0
+RY0 = 1.2
+
+VX0 = 0.8
+VY0 = 0.0
+
+
+def distToMoon(x, y):
+    return sqrt(x**2 + y**2)
+
+
+def getAttractionMag(x, y):
+    return G * MOON_MASS * SHIP_MASS / distToMoon(x, y)**2
+
+
+def eulerIntegration(rX, rY, vX, vY):
+    fAttr = getAttractionMag(rX, rY)
+    phi = atan2(rY, rX)
+
+    aX = -cos(phi) * fAttr / SHIP_MASS
+    aY = -sin(phi) * fAttr / SHIP_MASS
+
+    vX1 = vX + aX * DT
+    vY1 = vY + aY * DT
+
+    rX1 = rX + (vX + vX1) / 2 * DT
+    rY1 = rY + (vY + vY1) / 2 * DT
+
+    return rX1, rY1, vX1, vY1
+
+
+def simulation(rX, rY, vX, vY):
+    trajectory = [(rX, rY)]
+    isCrash = False
+
+    for i in range(STEPS_COUNT):
+        rX, rY, vX, vY = eulerIntegration(rX, rY, vX, vY)
+
+        trajectory.append((rX, rY))
+
+        isCrash = distToMoon(rX, rY) <= MOON_RADIUS
+        if isCrash:
+            break
+
+    return trajectory, isCrash
+
+
+def main(): 
+    trajectory, isCrash = simulation(RX0, RY0, VX0, VY0)
+
+    if isCrash:
+        print("CRASH!")
+
+    make_plot(MOON_RADIUS, [
+        [COLOR, trajectory],
+    ])
+
+
+if __name__ == "__main__":
+    main()
---
+++ model_1/plotter.py
@@ -0,0 +1,37 @@
+from PIL import Image, ImageDraw, ImageFont
+
+WIDTH = 1000
+HEIGHT = 1000
+
+SCALE = 100
+LINE_WIDTH = 3
+
+
+def draw_path(draw, pathData):
+    color, path = pathData[0], pathData[1]
+    
+    if (len(path) < 2):
+        return
+    
+    real_path = [(int(i[0] * SCALE) + WIDTH / 2, HEIGHT / 2 - int(i[1] * SCALE)) for i in path]
+
+    for i in range(1, len(real_path)):
+        draw.line((real_path[i], real_path[i - 1]), fill=color, width=LINE_WIDTH)
+
+
+def make_plot(planet_radius, paths):
+    image = Image.new('RGBA', (WIDTH, HEIGHT))
+    draw = ImageDraw.Draw(image)
+
+    draw.rectangle(((0, 0), (WIDTH, HEIGHT)), fill=(255, 255, 255))
+
+    real_radius = int(planet_radius * SCALE)
+    draw.ellipse((WIDTH / 2 - real_radius, HEIGHT / 2 - real_radius, WIDTH / 2 + real_radius, HEIGHT / 2 + real_radius), width=LINE_WIDTH, fill=(255, 255, 255), outline=(0, 0, 0))
+
+    draw.line(((0, HEIGHT / 2), (WIDTH, HEIGHT / 2)), fill=(0, 0, 0), width=LINE_WIDTH)  
+    draw.line(((WIDTH / 2, 0), (WIDTH / 2, HEIGHT)), fill=(0, 0, 0), width=LINE_WIDTH) 
+
+    for i in paths:
+        draw_path(draw, i)    
+
+    image.save('plot.png')
