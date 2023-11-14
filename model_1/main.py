ca659ff3065a62cc433dc6984c77f04ab63aeeda
Smoking_Elk <andre12go@mail.ru>
2023-11-14 18:45:26 +0300

    add engine model

--- model_1/main.py
+++ model_1/main.py
@@ -1,20 +1,24 @@
 from plotter import make_plot
 from math import sqrt, atan2, cos, sin
 
+# параметры симуляции
 MOON_RADIUS = 1
 MOON_MASS = 0.5
-SHIP_MASS = 0.02
 G = 1
+
+SHIP_MASS = 0.02
+ENGINE_WORKING_TIME = 0.03
+ENGINE_FORCE_MAG = 0.02
+FUEL_MASS0 = 0.0
+
+RX0 = 0
+RY0 = 1.0
+VX0 = 0.1
+VY0 = 0.1
 
 COLOR = (53, 178, 241)
 DT = 0.01
 STEPS_COUNT = 8000
-
-RX0 = 0
-RY0 = 1.2
-
-VX0 = 0.8
-VY0 = 0.0
 
 
 def distToMoon(x, y):
@@ -25,12 +29,20 @@ def getAttractionMag(x, y):
     return G * MOON_MASS * SHIP_MASS / distToMoon(x, y)**2
 
 
-def eulerIntegration(rX, rY, vX, vY):
+def eulerIntegration(rX, rY, vX, vY, fuelMass, fuelFlow):
+    fuelMass1 = max(0, fuelMass - fuelFlow * DT)
+    fullShipMass = SHIP_MASS + (fuelMass + fuelMass1) / 2
+
+    fEngine = ENGINE_FORCE_MAG 
+    if (fuelMass + fuelMass1) / 2 > 0:
+        fEngine = 0
+    alpha = atan2(vY, vX)
+
     fAttr = getAttractionMag(rX, rY)
     phi = atan2(rY, rX)
 
-    aX = -cos(phi) * fAttr / SHIP_MASS
-    aY = -sin(phi) * fAttr / SHIP_MASS
+    aX = (cos(alpha) * fEngine - cos(phi) * fAttr) / fullShipMass
+    aY = (sin(alpha) * fEngine - sin(phi) * fAttr) / fullShipMass
 
     vX1 = vX + aX * DT
     vY1 = vY + aY * DT
@@ -38,15 +50,17 @@ def eulerIntegration(rX, rY, vX, vY):
     rX1 = rX + (vX + vX1) / 2 * DT
     rY1 = rY + (vY + vY1) / 2 * DT
 
-    return rX1, rY1, vX1, vY1
+    return rX1, rY1, vX1, vY1, fuelMass1
 
 
-def simulation(rX, rY, vX, vY):
+def simulation(rX, rY, vX, vY, fuelMass, engineWorkingTime):
+    FUEL_FLOW = fuelMass / engineWorkingTime # удельный расход топлива
+    
     trajectory = [(rX, rY)]
     isCrash = False
 
     for i in range(STEPS_COUNT):
-        rX, rY, vX, vY = eulerIntegration(rX, rY, vX, vY)
+        rX, rY, vX, vY, fuelMass = eulerIntegration(rX, rY, vX, vY, fuelMass, FUEL_FLOW)
 
         trajectory.append((rX, rY))
 
@@ -58,7 +72,7 @@ def simulation(rX, rY, vX, vY):
 
 
 def main(): 
-    trajectory, isCrash = simulation(RX0, RY0, VX0, VY0)
+    trajectory, isCrash = simulation(RX0, RY0, VX0, VY0, FUEL_MASS0, ENGINE_WORKING_TIME)
 
     if isCrash:
         print("CRASH!")
