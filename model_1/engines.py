ae8f05172b3cf8d5f222061b604b13776d52f0bf
unmaxing <m4xskr@gmail.com>
2023-11-22 21:18:01 +0300

    Added engines.py

---
+++ model_1/engines.py
@@ -0,0 +1,63 @@
+class EngineBase:
+    def __init__(self, thrust):
+        self._thrust = thrust
+        self._isActive = False
+
+    def active(self):
+        self._isActive = True
+
+    def inactive(self):
+        self._isActive = False
+
+    def isActive(self):
+        return self._isActive
+
+    def getThrust(self):
+        return self._thrust if self.isActive() else 0
+    
+    def applyThrust(self, dt):
+        return self.getThrust(), 0
+    
+
+class CruiseEngine(EngineBase):
+    def __init__(self, thrust, fuelMass0, workingTime):
+        super().__init__(thrust)
+
+        self._fuelMass = fuelMass0
+        self._dFuelMassDt = fuelMass0 / workingTime
+        self._workingTime = workingTime
+
+    def getFuelMassDt(self):
+        return self._dFuelMassDt
+
+    def getFuelMass(self):
+        return self._fuelMass
+
+    def active(self):
+        if self.getFuelMass() > 0:
+            super().active()
+
+    def applyThrust(self, dt):
+        self._fuelMass = max(0, self.getFuelMass() - self.getFuelMassDt() * dt)
+
+        if self.getFuelMass() <= 0:
+            self.inactive()
+
+        return super().applyThrust(dt)
+    
+
+class ManeuveringThruster(EngineBase):
+    # direction:
+    # 1 - cw
+    # -1 - ccw
+    def __init__(self, thrust, height, direction):
+        super().__init__(thrust)
+
+        self._height = height
+        self._direction = direction
+
+    def getMoment(self):
+        return self._height * self._direction * self.getThrust() 
+
+    def applyThrust(self, dt):
+        return 0, self.getMoment()
--- model_1/main.py
+++ model_1/main.py
@@ -1,52 +1,73 @@
 from plotter import make_plot
 from math import sqrt, atan2, cos, sin
+from engines import CruiseEngine
 
 # параметры симуляции
+# сила притяжения
 MOON_RADIUS = 1
 MOON_MASS = 0.5
 G = 1
 
+
+CRASH_THRESHOLD = 0.1
 SHIP_MASS = 0.02
-ENGINE_WORKING_TIME = 4.5
-ENGINE_THRUST = 0.0008
-FUEL_MASS0 = 0.01
+
 
 RX0 = 0
 RY0 = 1.0
-VX0 = 0.7
-VY0 = 0.1
+VX0 = 0.0
+VY0 = 0.0
 
 COLOR_FUEL = (241,53,53)
 COLOR_FREE = (53, 178, 241)
 DT = 0.01
-SIMULATION_TIME = 40
+SIMULATION_TIME = 15
+
+
+def vecMag(x, y):
+    return sqrt(x**2 + y**2)
 
 
 def distToMoon(x, y):
-    return sqrt(x**2 + y**2)
+    return vecMag(x, y)
 
 
 def getAttractionMag(x, y, fullShipMass):
     return G * MOON_MASS * fullShipMass / distToMoon(x, y)**2
 
 
-def eulerIntegration(rX, rY, vX, vY, fuelMass, fuelFlow):
-    fuelMass1 = max(0, fuelMass - fuelFlow * DT)
-    fullShipMass = SHIP_MASS + (fuelMass + fuelMass1) / 2
+def isShipLanded(x, y, vX, vY):
+    return distToMoon(x, y) <= MOON_RADIUS and vecMag(vX, vY) < CRASH_THRESHOLD
 
-    # сила тяги двигателя
-    fEngine = ENGINE_THRUST 
-    if (fuelMass + fuelMass1) / 2 <= 0:
-        fEngine = 0
 
-    alpha = atan2(vY, vX)
+def eulerIntegration(rX, rY, vX, vY, shipOrientation, engines):
+    landed = isShipLanded(rX, rY, vX, vY)
+    fullShipMass = SHIP_MASS
+    fEngine = 0
+
+    for engine in engines:
+        fuelMass = engine.getFuelMass()
+        thrust, moment = engine.applyThrust(DT)
+        fuelMass1 = engine.getFuelMass()
+        fullShipMass += (fuelMass + fuelMass1) / 2
+        fEngine += thrust
 
     # сила притяжения
     fAttr = getAttractionMag(rX, rY, fullShipMass)
     phi = atan2(rY, rX)
 
-    aX = (cos(alpha) * fEngine - cos(phi) * fAttr) / fullShipMass
-    aY = (sin(alpha) * fEngine - sin(phi) * fAttr) / fullShipMass
+    # сила тяги двигателя
+
+
+    alpha = shipOrientation
+
+    # сила реакции опоры
+    fN = fAttr if landed else 0
+    
+    print(landed, fEngine, fN, fAttr)
+
+    aX = (cos(alpha) * fEngine + cos(phi) * (fN - fAttr)) / fullShipMass
+    aY = (sin(alpha) * fEngine + sin(phi) * (fN - fAttr)) / fullShipMass
 
     vX1 = vX + aX * DT
     vY1 = vY + aY * DT
@@ -54,27 +75,32 @@ def eulerIntegration(rX, rY, vX, vY, fuelMass, fuelFlow):
     rX1 = rX + (vX + vX1) / 2 * DT
     rY1 = rY + (vY + vY1) / 2 * DT
 
-    return rX1, rY1, vX1, vY1, fuelMass1
+    shipOrientation1 = shipOrientation
+
+    return rX1, rY1, vX1, vY1, shipOrientation1, fEngine
 
 
-def simulation(rX, rY, vX, vY, fuelMass, engineWorkingTime):
+def simulation(rX, rY, vX, vY, engines):
     # скорость расхода топлива
-    FUEL_FLOW = fuelMass / engineWorkingTime 
+
     STEPS_COUNT = int(SIMULATION_TIME / DT)
+
+    shipOrientation = atan2(rY, rX)
 
     trajectoryFuel = [(rX, rY)]
     trajectoryFree = []
     isCrash = False
 
     for i in range(STEPS_COUNT):
-        rX, rY, vX, vY, fuelMass = eulerIntegration(rX, rY, vX, vY, fuelMass, FUEL_FLOW)
+        rX, rY, vX, vY, shipOrientation, engineForces = eulerIntegration(rX, rY, vX, vY, shipOrientation, engines)
 
-        if fuelMass > 0:
+        if engineForces > 0:
             trajectoryFuel.append((rX, rY))
         else:
             trajectoryFree.append((rX, rY))
  
-        if distToMoon(rX, rY) <= MOON_RADIUS:
+        if distToMoon(rX, rY) <= MOON_RADIUS and not isShipLanded(rX, rY, vX, vY):
+            print(vecMag(vX, vY), distToMoon(rX, rY) <= MOON_RADIUS)
             isCrash = True
             break
     
@@ -84,8 +110,16 @@ def simulation(rX, rY, vX, vY, fuelMass, engineWorkingTime):
     return trajectoryFuel, trajectoryFree, isCrash
 
 
-def main(): 
-    trajectoryFuel, trajectoryFree, isCrash = simulation(RX0, RY0, VX0, VY0, FUEL_MASS0, ENGINE_WORKING_TIME)
+def main():
+    ENGINE_WORKING_TIME = 4.5
+    ENGINE_THRUST = 0.015
+    FUEL_MASS0 = 0.01
+
+    main_engine = CruiseEngine(ENGINE_THRUST, FUEL_MASS0, ENGINE_WORKING_TIME)
+
+    main_engine.active()
+    engines = [main_engine]
+    trajectoryFuel, trajectoryFree, isCrash = simulation(RX0, RY0, VX0, VY0, engines)
 
     if isCrash:
         print("CRASH!")
@@ -93,7 +127,7 @@ def main():
     make_plot(MOON_RADIUS, [
         [COLOR_FUEL, trajectoryFuel],
         [COLOR_FREE, trajectoryFree],
-    ], "./model_1/plot.png")
+    ], "plot.png")
 
 
 if __name__ == "__main__":
