098ac6d8bebaa7ad449e65961da3e4c00e32e06f
Smoking_Elk <andre12go@mail.ru>
2023-11-23 18:02:09 +0300

    add pid test

--- model_1/pid.py
+++ model_1/pid.py
@@ -5,6 +5,8 @@ class PidRegulator:
         self.kD = kD
         self.dt = dt
 
+        self.reset()
+
     def reset(self):
         self.prevErr = 0
         self.errIntegral = 0
@@ -17,3 +19,25 @@ class PidRegulator:
         dErrDt = dErr / self.dt
 
         return self.kP * err + self.kI * self.errIntegral + self.kD * dErrDt
+    
+
+if __name__ == "__main__":
+    from time import sleep
+    from math import sin
+
+    DT = 0.1
+    pid = PidRegulator(0.0001, 0.0001, 0.99, DT)
+
+    current = 0
+    v = 0
+    target = 20
+    t = 0
+
+    while True:
+        sleep(DT)
+        
+        target = 20 + 2 * sin(t / 10)
+        v += pid.control(target - current) * DT
+        current += v * DT
+        t += DT
+        print(f"{current:.10f} {target:.10f} {abs(target - current):.10f}")
