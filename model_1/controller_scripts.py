c5634d21542bf5bdf3caf8dfca09096b6c6e44bb
Smoking_Elk <andre12go@mail.ru>
2023-12-03 18:10:25 +0300

    add autopilot v2

--- model_1/controller_scripts.py
+++ model_1/controller_scripts.py
@@ -19,12 +19,12 @@ def getAngleDifference(alpha, beta):
     return phi * sgn
 
 
-class MainScript(ScriptBase):
+class MainScriptBase(ScriptBase):
     def __init__(self):
+        super().__init__()
+
         self.stage = 0
         self._isFreeFlight = False
-
-        self.initialAnglePID = PidRegulator(4, 0, 2, DT)
 
     def isFreeFlight(self):
         return self._isFreeFlight
@@ -35,6 +35,16 @@ class MainScript(ScriptBase):
     def thrustersControl(self, thrusterLeft, thrusterRight, control):
         thrusterLeft.setThrustLevel(control)
         thrusterRight.setThrustLevel(-control)
+
+    def update(self, model, metrics):
+        return super().update(model, metrics)
+
+
+class MainScriptV1(MainScriptBase):
+    def __init__(self):
+        super().__init__()
+
+        self.initialAnglePID = PidRegulator(4, 0, 2, DT)
 
     def update(self, model, metrics, log):
         time = metrics["t"]
@@ -76,6 +86,60 @@ class MainScript(ScriptBase):
                 self.thrustersControl(thrusterLeft, thrusterRight, controlSignal)
                 
             case 3:
+                self.thrustersControl(thrusterLeft, thrusterRight, 0)
+                pass
+
+
+class MainScriptV2(MainScriptBase):
+    def __init__(self):
+        super().__init__()
+
+        self.initialAnglePID = PidRegulator(64, 0, 2, DT)
+
+    def update(self, model, metrics, log):
+        time = metrics["t"]
+
+        mainEngine = model["main_engine"]
+        thrusterLeft = model["thruster_left"]
+        thrusterRight = model["thruster_right"]
+
+        alpha = metrics["shipOrientation"]
+        distToMoon = sqrt(metrics["rX"]**2 + metrics["rY"]**2)
+        height = distToMoon - MOON_RADIUS
+
+        targetAlpha = atan2(-metrics["rX"] / distToMoon, metrics["rY"] / distToMoon)
+
+        match self.stage:
+            case 0:
+                mainEngine.active()
+                mainEngine.setThrustLevel(1)
+
+                thrusterLeft.active()
+                thrusterLeft.setThrustLevel(0)
+
+                thrusterRight.active()
+                thrusterRight.setThrustLevel(0)
+
+                log(f"Main engine has been activaited at {time} (stage 1)")
+                self.stage = 1
+            case 1:
+                if height >= SAFE_HEIGHT:
+                    log(f"Safe height has been reached at {time} (stage 2)")
+                    self.stage = 2
+            case 2:
+                angleError = getAngleDifference(alpha, targetAlpha)
+
+                if mainEngine.getFuelMass() <= 0:
+                    log(f"Out of fuel at {time} (stage 3)")
+                    self.stage = 3
+                    mainEngine.setThrustLevel(0)
+                    self._isFreeFlight = True
+                
+                controlSignal = self.initialAnglePID.control(angleError)
+                self.thrustersControl(thrusterLeft, thrusterRight, controlSignal)
+                
+            case 3:
+                self.thrustersControl(thrusterLeft, thrusterRight, 0)
                 pass
 
 
